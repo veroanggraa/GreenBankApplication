1bfd8eee3ea1a87c7f5977124b39f6ea599c456a
veroanggraa <anggrainiveronica227@gmail.com>
2025-02-03 21:21:09 +0700

    update main activity

--- app/src/main/java/com/veroanggra/greenbankapplication/MainActivity.kt
+++ app/src/main/java/com/veroanggra/greenbankapplication/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.veroanggra.greenbankapplication.component.SavingCard
+import androidx.compose.ui.unit.dp
 import com.veroanggra.greenbankapplication.ui.theme.GreenBankApplicationTheme
+import com.veroanggra.greenbankapplication.utils.actions.setClipboard
+import com.veroanggra.greenbankapplication.utils.component.SavingCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +17,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GreenBankApplicationTheme {
-                SavingCard(modifier = Modifier)
+                SavingCard(
+                    modifier = Modifier.padding(horizontal = 30.dp, vertical = 60.dp),
+                    onClick = {
+                        this.setClipboard("label", "text")
+                    })
             }
         }
     }
