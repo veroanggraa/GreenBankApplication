a1aa5f6943589252d09758c8386ab1a3a4de6dbd
veroanggraa <anggrainiveronica227@gmail.com>
2025-02-19 16:12:15 +0700

    remove package

--- app/src/main/java/com/veroanggra/greenbankapplication/MainActivity.kt
+++ app/src/main/java/com/veroanggra/greenbankapplication/MainActivity.kt
@@ -14,35 +14,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             GreenBankApplicationTheme {
                 HomeScreen(this)
-
-//                Column {
-//                    GrayLine(Modifier, Color(0xFFF3F4F6), 50f)
-//                    Spacer(modifier = Modifier.height(20.dp))
-//                    SavingCard(
-//                        modifier = Modifier.padding(horizontal = 30.dp, vertical = 60.dp),
-//                        textNumber = textNumber,
-//                        textName = textName,
-//                        balance = balance,
-//                        onClick = {
-//                            this.apply {
-//                                setClipboard("Account Number", textNumber)
-//                                isShowMessage = true
-//                                message = "Berhasil di-Copy!"
-//                            }
-//                        },
-//                        isVisible = isVisible,
-//                        eyeClick = {
-//                            coroutineScope.launch {
-//                                BankingDataStore.saveIsVisible(this@MainActivity, !isVisible)
-//                            }
-//                        })
-//                }
-//                if (isShowMessage) {
-//                    CustomOnTopToast(
-//                        message = message,
-//                        onDismiss = { isShowMessage = false }
-//                    )
-//                }
             }
         }
     }
